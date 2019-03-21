d4017126c6cec2c4b42373bdf813777261169376
hhohha <hhohha@seznam.cz>
2019-03-21 17:25:42 +0100

    .

--- ts.hs
+++ ts.hs
@@ -5,6 +5,7 @@ import System.Environment
 import System.Directory
 import System.IO
 
+-- symbol of the grammar
 type Symbol = String
 
 -- non-terminal and list of symbols on the right side
@@ -12,7 +13,6 @@ type Rule = (Symbol, [Symbol])
 
 -- tuple of non-terminal and it's set of reachable symbols by single rules
 type NSet = (Symbol, [Symbol])
-
 
 -- grammar is: (1.) set of non-terminals, (2.) set of terminals
 -- (3.) starting non-terminal, (4.) set of rules
@@ -25,6 +25,10 @@ data Grammar = Grammar {
 
 -- define how to stringify grammar
 instance Show Grammar where show = showGrammar
+
+-- print the rule
+showRule :: Rule -> String
+showRule r = fst r ++ "->" ++ (concat $ snd r)
 
 -- stringify the grammar
 showGrammar :: Grammar -> String
@@ -42,21 +46,14 @@ newGrammar nts ts start rs
     | not (all isUpper $ concat nts) = error "wrong input format of the non-terminals"
     | not (all isLower $ concat ts) = error "wrong input format of the terminals"
     | not (start `elem` nts) = error "wrong input format, unknown starting symbol"
-    | not (validateRules rs nts ts) = error "wrong input format of the rules"
+    | not (all validateRule rs) = error "wrong input format of the rules"
     | otherwise = Grammar nts ts start rs
+    where validateRule r = fst r `elem` nts && intersect (snd r) (nts ++ ts) == snd r
 
 -- is used for creating transformad grammars
 -- creates grammar with no checks (the format of the transformed grammar can be different anyway)
 newGrammarNoChecks :: [Symbol] -> [Symbol] -> Symbol -> [Rule] -> Grammar
 newGrammarNoChecks nts ts start rs = Grammar nts ts start rs
-
--- checks the input format of the rules
-validateRules :: [Rule] -> [Symbol] -> [Symbol] -> Bool
-validateRules [] _ _ = True
-validateRules (x:xs) nts ts =
-    fst x `elem` nts
-    && intersect (snd x) (nts ++ ts) == snd x
-    && validateRules xs nts ts
 
 -- generates new grammar with transformed rules so that simple rules are eliminated
 removeSimpleRules :: Grammar -> Grammar
@@ -65,6 +62,8 @@ removeSimpleRules g = newGrammarNoChecks
     (terms g)
     (start g)
     (transformRules (rules g) (getReachableNonTerms (nterms g) (rules g)))
+    where getReachableNonTerms [] _ = []
+          getReachableNonTerms (nt:nts) rs = [(nt, findSetN [nt] rs)] ++ getReachableNonTerms nts rs
 
 -- applies NSet - the set of rules reachable by simple rules
 transformRules :: [Rule] -> [NSet] -> [Rule]
@@ -74,17 +73,11 @@ transformRules (r:rs) sets =
     then []
     else generateNewRules r (map fst (filter (\x -> (fst r) `elem` (snd x)) sets)))
     ++ transformRules rs sets
+    where generateNewRules r [] = []
+          generateNewRules r (x:xs) = [(x, snd r)] ++ generateNewRules r xs
 
-generateNewRules :: Rule -> [Symbol] -> [Rule]
-generateNewRules r [] = []
-generateNewRules r (x:xs) = [(x, snd r)] ++ generateNewRules r xs
-
--- finds for every non-terminal a NSet - set of non-terminals
--- which are reachable by applications of simple rules
-getReachableNonTerms :: [Symbol] -> [Rule] -> [NSet]
-getReachableNonTerms [] _ = []
-getReachableNonTerms (nt:nts) rs = [(nt, findSetN [nt] rs)] ++ getReachableNonTerms nts rs
-
+-- for every non-terminal symbol find all non-terminal symbols,
+-- that are reachable by simple rules
 findSetN :: [Symbol] -> [Rule] -> [Symbol]
 findSetN symbs rs =
     if symbs == nextIteration
@@ -92,6 +85,7 @@ findSetN symbs rs =
     else findSetN nextIteration rs
         where nextIteration = findSetNStep symbs rs
 
+-- one iteration of findSetN
 findSetNStep :: [Symbol] -> [Rule] -> [Symbol]
 findSetNStep symbs [] = symbs
 findSetNStep symbs (r:rs) =
@@ -107,21 +101,19 @@ findSetNStep symbs (r:rs) =
 isRuleSimple :: Rule -> Bool
 isRuleSimple r = length (snd r) == 1 && all isUpper (head $ snd r)
 
----------------------------------------------------------
+-- removes duplicate values from a list
+removeDuplicates :: Ord b => [b] -> [b]
+removeDuplicates x = (map head . group . sort) x
 
+-- takes a grammar and performs transformation to CNF
 transformToCNF :: Grammar -> Grammar
 transformToCNF g = newGrammarNoChecks
-    (getNonTerminals  (transformRulesToCNF (rules g)) (nterms g))
+    (getNonTerminals (transformRulesToCNF g) (nterms g))
     (terms g)
     (start g)
-    (transformRulesToCNF (rules g))
-
-getNonTerminals :: [Rule] -> [Symbol] -> [Symbol]
-getNonTerminals rs symbs = map head $ group $ sort (map fst rs ++ symbs)
-
-transformRulesToCNF :: [Rule] -> [Rule]
-transformRulesToCNF [] = []
-transformRulesToCNF (r:rs) = parseRule r ++ transformRulesToCNF rs
+    (transformRulesToCNF g)
+        where transformRulesToCNF g = removeDuplicates $ concat $ map parseRule $ rules g
+              getNonTerminals rs symbs = removeDuplicates $ (map fst rs ++ symbs)
 
 -- takes one rule and generates corresponding rules based on the CNF algorithm
 parseRule :: Rule -> [Rule]
@@ -145,7 +137,7 @@ parseRule r
         ++ parseComposedNonTerm (concat $ tail $ snd r) 
         
     -- there should be no other type of rule
-    | otherwise = error ("bad rule: " ++ show r)
+    | otherwise = error ("bad rule: " ++ showRule r)
 
 -- parse the non-terminal in form <abcd> and return list of rules
 parseComposedNonTerm :: String -> [Rule]
@@ -174,35 +166,45 @@ generateTerminalRule s = if all isLower s then [(s ++ "'", [s])] else []
 commify :: Symbol -> Symbol
 commify s = if all isLower s then s ++ "'" else s
 
+-- load the grammar from a string
 readGrammarFromStr :: [String] -> Grammar
 readGrammarFromStr (l1:l2:l3:ls) =
     newGrammar (splitOn "," l1) (splitOn "," l2) l3
         (map (\x -> (head x, map (:[]) $ concat (tail x))) (map (splitOn "->") ls))
 readGrammarFromStr _ = error "bad input format"
 
+-- command -i: load and print the grammar
 readAndPrintStr :: String -> String
 readAndPrintStr s = show $ readGrammarFromStr $ lines s
 
+-- command -1: load the grammar, remove simple rules and print
 removeSimpleRulesStr :: String -> String
 removeSimpleRulesStr s = show $ removeSimpleRules $ readGrammarFromStr $ lines s
 
+-- command -2: load the grammar and transform to CNF
 transformToCNFStr :: String -> String
 transformToCNFStr s = show $ transformToCNF $ removeSimpleRules $ readGrammarFromStr $ lines s
 
+-- map of known commands and assigned functions
 dispatch :: [(String, String -> String)]
 dispatch =  [("-i", readAndPrintStr), ("-1", removeSimpleRulesStr), ("-2", transformToCNFStr)]
 
 main = do
     args <- getArgs
+
+    -- expecting a command and possibly input file
     if length args < 1 || length args > 2
     then putStrLn "bad arguments"
     else do
+        -- read the command
         let command = head args
 
+        -- read the input from file or stdin
         contents <- (if length args == 2
                      then readFile $ last args
                      else getContents)
 
+        -- execute the command
         case lookup command dispatch of
             Nothing -> putStrLn "bad arguments"
             Just a -> putStrLn $ a contents
